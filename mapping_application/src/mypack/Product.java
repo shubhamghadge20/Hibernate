9bdc5c4e3d02ecb346ffdb73302040ef684b4b86
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-04-17 18:50:08 +0530

    hibernate_application using mapping (hbm)

---
+++ mapping_application/src/mypack/Product.java
@@ -0,0 +1,28 @@
+package mypack;
+
+public class Product {
+  
+	private int pcode;
+	private String pname;
+	private float prate;
+	public int getPcode() {
+		return pcode;
+	}
+	public void setPcode(int pcode) {
+		this.pcode = pcode;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public float getPrate() {
+		return prate;
+	}
+	public void setPrate(float prate) {
+		this.prate = prate;
+	}
+	
+	
+}
---
+++ mapping_application/src/mypack/ProductMain.java
@@ -0,0 +1,34 @@
+package mypack;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ProductMain {
+
+	public static void main(String[] args) {
+		Configuration c=new Configuration();
+		c.configure("hibernate.cfg.xml");
+		SessionFactory sf=c.buildSessionFactory();
+		Session s=sf.openSession();
+		Transaction t=s.beginTransaction();
+			
+		
+		Product p=new Product();
+		p.setPcode(102);
+		p.setPname("Pencil");
+		p.setPrate(20.0f);
+		
+		s.persist(p);			//save an object
+		t.commit();
+		s.close();
+		System.out.println("Done..");
+		
+		
+	
+	}
+
+}
