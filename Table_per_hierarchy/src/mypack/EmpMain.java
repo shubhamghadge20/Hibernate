85468496b6fedfe8e8a814faf933ebee504b0ec8
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-04-22 19:51:55 +0530

    table per hierarchy

---
+++ Table_per_hierarchy/src/mypack/EmpMain.java
@@ -0,0 +1,41 @@
+package mypack;
+//Hibernate Inheritance - Table per concrte class
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		Configuration c=new Configuration();
+		c.configure("hibernate.cfg.xml");
+		SessionFactory factory=c.buildSessionFactory();
+		Session session= factory.openSession();
+		Transaction t=session.beginTransaction();
+		
+		Employee e=new Employee();
+		e.setEname("Shreyashi");
+		
+		Regular_Emp re=new Regular_Emp();
+		re.setEname("Raghav");
+		re.setSalary(20000);
+		re.setBonus(5000);
+		
+		Contract_Emp ce=new Contract_Emp();
+		ce.setEname("Sham");
+		ce.setPay_per_hour(2000);
+		ce.setContract_duration(3);
+		
+		session.persist(e);
+		session.persist(re);
+		session.persist(ce);
+		t.commit();
+		session.close();
+		System.out.println("Commit");
+	
+
+
+	}
+
+}
---
+++ Table_per_hierarchy/src/mypack/Regular_Emp.java
@@ -0,0 +1,19 @@
+package mypack;
+
+public class Regular_Emp extends Employee {
+	private int bonus;
+	private int salary;
+	public int getBonus() {
+		return bonus;
+	}
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
